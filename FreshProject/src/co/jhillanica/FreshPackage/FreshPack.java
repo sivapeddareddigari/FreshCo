4562994c7931e50f52121ec05aaa629a7124edac
sivapeddareddigari <s_h_i_v_a@outlook.com>
2017-08-25 23:56:38 -0400

    Initial commit

---
+++ FreshProject/src/co/jhillanica/FreshPackage/FreshPack.java
@@ -0,0 +1,11 @@
+package co.jhillanica.FreshPackage;
+
+public class FreshPack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Line");
+		System.out.println("Hello Second Line");
+	}
+
+}
---
+++ FreshProject/src/co/jhillanica/FreshPackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author s_h_i
+ *
+ */
+package co.jhillanica.FreshPackage;
